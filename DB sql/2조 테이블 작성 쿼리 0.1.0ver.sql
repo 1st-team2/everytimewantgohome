2761ed95fb136043659881c90c7d4e3da1f92b74
leenara02 <nala016@naver.com>
2024-04-11 09:31:51 +0900

    sql오타 수정

--- DB sql/2조 테이블 작성 쿼리 0.1.0ver.sql
+++ DB sql/2조 테이블 작성 쿼리 0.1.0ver.sql
@@ -17,11 +17,11 @@ CREATE TABLE users(
 CREATE TABLE boards(
 	no					INT									PRIMARY KEY AUTO_INCREMENT
 	,user_id			INT				NOT NULL			
-	,date_at			DATE()			NOT NULL
+	,date_at			DATE			NOT NULL
 	,title			VARCHAR(50)		NOT NULL
 	,content			VARCHAR(1000)	NOT NULL
 	,created_at		DATETIME			NOT NULL			DEFAULT CURRENT_TIMESTAMP
 	,deleted_at		DATETIME			NULL				DEFAULT CURRENT_TIMESTAMP
 	,done				INT				NOT NULL			DEFAULT 0
-	FOREIGN KEY (user_id) REFERENCES users(id)
+	,FOREIGN KEY (user_id) REFERENCES users(id)
 	);
