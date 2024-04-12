6c50b3dbead1dfe9af034f0f7af72caea26e745a
minsea1256 <minsea1256@gmail.com>
2024-04-12 10:58:11 +0900

    list_ms "게시글없음" 수정 완료_권민서

--- src/list_ms.php
+++ src/list_ms.php
@@ -1,36 +1,36 @@
-<?php
-// <!-- 
-//     v. 1.0.1
-//     작성일자 : 2024-04-04 오전 11시
-//     작성(수정)자 : 이나라
-//     작성(수정)내용 : div클래스명 규칙
-//     v. 1.0.2
-//     작성일자 : 2024-04-05 오후 2시
-//     작성(수정)자 : 노경호
-//     작성(수정)내용 : list페이지 명명규칙
-
-//     클래스명
-//     1. 'TODO LIST' : header
-//     2. 창 부분 : main
-//     3. 창 상단 최소화,전체화면,뒤로가기(이미지수정)부분 : main_top
-//     3-1. 날짜 들어갈곳 : top_date
-//     3-2. 최소화(-) : minus
-//     3-3. 네모(ㅁ) : square
-//     3-4. 뒤로가기(x) : back 
-//     4. 창 화면 부분 : main_mid
-//     5. 게이지,달력 부분 : main_left
-//     6. 이름,프사 부분 : main_right
-//     7. 게이지 이름 : gauge_name
-//     8. 게이지 부분 : gauge_bar
-//     9. 달력 이름 : cal_name
-//     10. 달력 : cal
-//     11. 닉네임 부분 : nick_name
-//     12. 프사부분 : personal_img
-//  -->
-require_once( $_SERVER["DOCUMENT_ROOT"]."/config.php"); // 설정 파일 호출
+<?php 
+require_once( $_SERVER["DOCUMENT_ROOT"]."/config_nr.php"); // 설정 파일 호출
 require_once(FILE_LIB_DB); // DB관련 라이브러리
 $list_cnt = 100; // 한 페이지 최대 표시 수
 $page_num = 1; // 페이지 번호 초기화
+
+//날짜 관련 - last update : 노경호 0409
+//0411 오후2시 55분 함수 추가, 이미지변경시작 : 이나라 / 오후 3시40분 완료
+
+//리스트 날짜 url에서 가져오기
+// $date = $_GET['date'];
+$date = isset($_GET['date']) ? $_GET['date'] : date('Y-m-d');
+
+
+//현재날짜 가져오기
+$current_date = date('Y-m-d');
+
+// 이전 날짜 계산 (하루 전)
+$previous_date = date('Y-m-d', strtotime($date . ' -1 day'));
+
+// 다음 날짜 계산 (하루 후)
+$next_date = date('Y-m-d', strtotime($date . ' +1 day'));
+
+// nr - 이미지 가져오는 함수
+function db_select_img(&$conn) {
+    //sql
+    $sql = " SELECT img FROM select_img WHERE id = 1 ";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+    return $result;
+}
 
 try {
     // DB Connect
@@ -56,16 +56,19 @@ try {
     $max_page_num = ceil($result_board_cnt / $list_cnt); // 최대 페이지 수
     $offset = $list_cnt * ($page_num -1); // 오프셋
 
-
     // 게시글 리스트 조회
     $arr_param = [
         "list_cnt" => $list_cnt
         ,"offset" => $offset
+        ,"target_date" => $date
     ];
     $result = db_select_boards_paging($conn, $arr_param);
 
     // 아이템 셋팅
-    $item = $result[0];
+    $item = $result;
+
+    $img_result = db_select_img($conn);
+    $img = $img_result[0]["img"];
 
 } catch(\Throwable $e) {
     echo $e->getMessage();
@@ -77,64 +80,75 @@ try {
     }
 }
 
-
 ?>
-
  <!DOCTYPE html>
 <html lang="ko">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/list_ms.css">
-    
-    <title>TODO LIST</title>
+    <title>Daily List</title>
 </head>
 <body>
-    <a href="./main01.php"><div class="header">TODO LIST</div></a>
+    <a href="./main01.php"><div class="header">PIXEL FOREST</div></a>
     <div class="main">
         <div class="main_top"> <!--이미지로 대체-->
-            <div class="top_date">2024-04-04</div>
+            <div class="top_date">NOW DATE :<?php echo $current_date ?></div>
             <div class="minus">-</div>
             <div class="square">ㅁ</div>
             <div class="back"><a href="./main01.php">x</a></div>
         </div>
         <div class="main_mid">
             <div class="main_left">
+                <form action="./insert.php" method="get">
+                <input type="hidden" name="date" value="<?php echo htmlspecialchars($date); ?>">
                 <div class="main_list_button">
                     <div class="add_button_box">
-                        <a class="add_button" href="./insert.php">Add</a>
+                        <!-- <a class="add_button" href="./insert.php?date="> -->
+                            <button type="submit">
+                                Add
+                            </button>
+                    <!-- </a> -->
                     </div>
+                </form>
                 </div>
                 <div class="list_container" >
                     <div class="list_items" >
-                        <?php
-                            foreach($result as $item) {
-                        ?>
-                            <form action="./chk_ms.php" method="post">
-                                <div class="daily_list">
-                                    <label class="chk_label <?php echo isset($item["checked_at"]) ? "chk-label-checked" : "" ?>" for="chk_label<?php echo $item["no"];?>"><?php echo isset($item["checked_at"]) ? "✔" : "" ?></label>
-                                    <button type="submit" id="chk_label<?php echo $item["no"];?>">a</button>
-                                    <div class="itme-button-a"><a href="./update.php?no=<?php echo $item["no"]?>" class="<?php echo isset($item["checked_at"]) ? "color" : "" ?>"><?php echo $item["title"] ?></a></div>
-                                </div>
-                            <input type="hidden" name="no" value="<?php echo $item["no"]; ?>">
-                        </form>
-                        <?php
-                            }
-                        ?>
-                    </div>
+                    <?php
+                    if(!empty($result)) {
+                        foreach($result as $item) {
+                        
+                    ?>
+                        <form action="./chk_ms.php" method="post">
+                        <input type="hidden" name="date" value="<?php echo htmlspecialchars($date); ?>">
+                            <div class="daily_list">
+                                <label class="chk_label <?php echo isset($item["checked_at"]) ? "chk-label-checked" : "" ?>" for="chk_label<?php echo $item["no"];?>"><?php echo isset($item["checked_at"]) ? "✔" : "" ?></label>
+                                <button type="submit" id="chk_label<?php echo $item["no"];?>"></button>
+                                <div class="item-button-a"><a href="./detail.php?date=<?php echo $date ?>&no=<?php echo $item["no"]?>" class="<?php echo isset($item["checked_at"]) ? "color" : "" ?>"><?php echo $item["title"] ?></a></div>
+                            </div>
+                        <input type="hidden" name="no" value="<?php echo $item["no"]; ?>">
+                    </form>
+                    <?php
+                        }
+                        }else {
+                    ?>
+                        <div class="itme_list"><?php echo "게시글이 없습니다."; ?></div>
+                    <?php
+                        }
+                    ?>
                 </div>
             </div>
             <div class="main_right">
-                <form action="" method="post" enctype="multipart/form-data" class="img_add" >
-                    <div class="personal_img">
-                        <input type="file" name="personal_img" id="personal_img">
-                    </div>
-                    <button type="submit">OK</button>
-                </form>
-                <img src="../image/personal.png" alt="" class="img_p">
-                <form action="./lis_mst/php" method="post">
+                <img src="<?php echo $img ?>" alt=""class="img_p">
+                <form action="" method="post">
                     <div class="nick_name_item">
-                        <div class="nick_date_item"></div>
+                        <div class="date_controll">
+                            <a href="list.php?date=<?php echo $previous_date; ?>"><</a>
+                        </div>
+                        <div><?php echo $date ?></div>
+                        <div class="date_controll">
+                        <a href="list.php?date=<?php echo $next_date; ?>">></a>
+                        </div>
                     </div>
                 </form>
             </div>
